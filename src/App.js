f25be1d662ad3927b756593062528cfd98636f83
Jacob Hoopes <rjacobhoopes@gmail.com>
2024-01-17 16:25:24 -0500

    Initial commit of my react app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Home from './Home';
+// import About from './About' ;
+// import Contact from './Contact';
+
+// const Home = () => <div>Home Page</div>;
+// const About = () => <div>About Page</div>;
 
 function App() {
   return (
+    // <Router>
+    //   <Switch>
+    //     <Route path="/" exact component={Home} />
+    //     <Route path="/about" exact component={About} />
+    //     <Route path="/contact" exact component={Contact} />
+    //   </Switch>
+    // </Router>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
